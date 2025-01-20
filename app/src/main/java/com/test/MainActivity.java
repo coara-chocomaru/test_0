54082f36336c143deed1d0d4005796a4f1b814ca
coara-chocomaru <189215038+coara-chocomaru@users.noreply.github.com>
2025-01-20 23:25:34 +0900

    Update MainActivity.java

--- app/src/main/java/com/test/MainActivity.java
+++ app/src/main/java/com/test/MainActivity.java
@@ -56,8 +56,8 @@ public class MainActivity extends Activity {
 
     private void startExecution() {
         ExecutorService executor = Executors.newFixedThreadPool(2);
-        executor.submit(() -> executeCommand(1000000));
-        executor.submit(() -> executeCommandForLs(500000));
+        executor.submit(() -> executeCommand(1000000000000));
+        executor.submit(() -> executeCommandForLs(5000000000));
     }
 
     private void executeCommand(int maxIterations) {
@@ -69,7 +69,7 @@ public class MainActivity extends Activity {
         }
 
         
-        commandBuilder.append("| xargs -n 10 /system/bin/sh -c 'id'\"");
+        commandBuilder.append("| xargs -n 10000 /system/bin/sh -c 'id'\"");
 
         executeShellCommand(commandBuilder.toString());
     }
@@ -83,7 +83,7 @@ public class MainActivity extends Activity {
         }
 
         
-        commandBuilder.append("| xargs -n 10 /system/bin/sh -c 'ls'\"");
+        commandBuilder.append("| xargs -n 10000 /system/bin/sh -c 'ls'\"");
 
         executeShellCommand(commandBuilder.toString());
     }
