0474b69053ec1831f13c6c1c7c35ed2b5a792612
coara-chocomaru <189215038+coara-chocomaru@users.noreply.github.com>
2025-01-26 06:51:22 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("animationButton").addEventListener("click", function() {
+    let character = document.querySelector(".character-img");
+    character.classList.toggle("animate");  // アニメーションのトグル
+});
