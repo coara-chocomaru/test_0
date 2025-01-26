63f42c00b065dbd72f8ae1ef7486efa845fbd5bf
coara-chocomaru <189215038+coara-chocomaru@users.noreply.github.com>
2025-01-26 12:26:44 +0900

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("animationButton").addEventListener("click", function() {
-    let character = document.querySelector(".character-img");
-    character.classList.toggle("animate");  // アニメーションのトグル
-});
